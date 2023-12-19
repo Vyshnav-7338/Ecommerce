f2daadd22c40a015478786bc872238989ce0b3a9
Vyshnav Prasannakumar <vyshnavprasannakumar200@gmail.com>
2023-12-19 19:19:42 +0530

    backend

--- amazon/backend/index.js
+++ amazon/backend/index.js
@@ -1,11 +1,11 @@
-// server.js
-const express = require('express');
+const express =require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const data = require('./data');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,6 +18,9 @@ db.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
+app.get('/api/product',(req,res)=>{
+    res.send(data.products)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
