76dc3fafe73d5024208758c4280a997900662918
Vyshnav Prasannakumar <vyshnavprasannakumar200@gmail.com>
2023-12-19 18:28:32 +0530

    add static Products

--- amazon/frontend/src/App.jsx
+++ amazon/frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import data from "./data";
 
 function App() {
   return (
@@ -7,10 +8,26 @@ function App() {
         <a href="/">amazon</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}/-</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
