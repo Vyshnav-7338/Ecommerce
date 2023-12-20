96183a967cb357c2103e5d7cf93b009563f46bd0
Vyshnav Prasannakumar <vyshnavprasannakumar200@gmail.com>
2023-12-20 15:36:51 +0530

    Add product

--- amazon/backend/index.js
+++ amazon/backend/index.js
@@ -1,8 +1,8 @@
-const express =require('express')
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const data = require('./data');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const data = require("./data");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,17 +11,25 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost/mern_project');
+mongoose.connect("mongodb://localhost/mern_project");
 
 const db = mongoose.connection;
-db.once('open', () => {
-    console.log('Connected to MongoDB');
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
-app.get('/api/product',(req,res)=>{
-    res.send(data.products)
-})
+app.get("/api/product", (req, res) => {
+  res.send(data.products);
+});
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
